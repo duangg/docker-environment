bc38e6f33b741df7e6a6e020a81948bd9d9a6874
yangyu <yangyu@meicai.cn>
2022-03-09 10:56:03 +0800

    add command examples

--- cmd-examples.sh
+++ cmd-examples.sh
@@ -2,8 +2,11 @@
 # some alias
 alias composer='docker run --rm -v $(pwd):/app -v $HOME/.composer:/root/.composer php:local composer'
 alias dc='docker-compose'
-# start composer
+# start compose
 dc up -d
 dc start
 dc stop
 dc down
+
+cd php_project
+composer install
